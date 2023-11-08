ca73d7a00836d80fa31736be857f91fc6a2e6ee8
ahmetvuruskan <ahmetvrskn@gmail.com>
2023-11-08 18:29:01 +0300

    refactored useEffect to action creator function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useEffect} from "react";
+import Notification from "./components/UI/Notification";
+import {Fragment, useEffect} from "react";
+import {sendCartData} from "./store/cart-slice";
 
+let initial = true;
 function App() {
+    const dispatch = useDispatch();
     const showCart = useSelector(state => state.ui.cartIsVisible);
+    const notification = useSelector(state => state.ui.notification);
     const cart = useSelector(state => state.cart);
+
     useEffect(() => {
-       fetch("https://react-http-example-9c3e4-default-rtdb.europe-west1.firebasedatabase.app/cart.json",{
-            method: 'PUT',
-            body: JSON.stringify(cart)
-        })
-    }, [cart]);
-  return (
-    <Layout>
-        {showCart && <Cart />}
-      <Products />
-    </Layout>
-  );
+        if (initial){
+            initial = false;
+            return;
+        }
+        dispatch(sendCartData(cart));
+
+
+    }, [cart, dispatch]);
+    return (
+        <Fragment>
+            {notification && <Notification
+                status={notification.status}
+                title={notification.title}
+                message={notification.message}/>}
+            <Layout>
+                {showCart && <Cart/>}
+                <Products/>
+            </Layout>
+        </Fragment>
+    );
 }
 
 export default App;
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
-
+import {uiActions} from "./ui-slice"
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {items: [], totalQuantity: 0, changed: false},
@@ -40,6 +40,39 @@ const cartSlice = createSlice({
         }
     }
 });
+
+export const sendCartData = (cartData) =>{
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({
+            status: 'pending',
+            title: 'Sending...',
+            message: 'Sending cart data!'
+        }));
+        const sendRequest = async () => {
+            const response = await fetch('https://react-http-example-9c3e4-default-rtdb.europe-west1.firebasedatabase.app//cart.json',{
+                method: 'PUT',
+                body: JSON.stringify(cartData)
+            });
+            if (!response.ok){
+                throw new Error('Sending cart data failed!');
+            }
+        }
+        try {
+            await sendRequest();
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success!',
+                message: 'Sent cart data successfully!'
+            }));
+        } catch (error) {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart data failed!'
+            }));
+        }
+    }
+}
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
