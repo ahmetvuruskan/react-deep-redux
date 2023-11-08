9bdee4f6369d028422759b4be09a614c73bbf70d
ahmetvuruskan <ahmetvrskn@gmail.com>
2023-11-08 17:45:51 +0300

    feature: added firebase connection

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,8 +1,15 @@
 import classes from './CartItem.module.css';
-
+import {cartActions } from '../../store/cart-slice';
+import {useDispatch} from "react-redux";
 const CartItem = (props) => {
-  const { name, quantity, totalPrice, price } = props.item;
-  console.log(props.item)
+  const dispatch = useDispatch();
+  const { name, quantity, totalPrice, price,id } = props.item;
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  }
+    const addItemHandler = () => {
+        dispatch(cartActions.addItemToCart(props.item));
+    }
 
   return (
     <li className={classes.item}>
@@ -18,8 +25,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
